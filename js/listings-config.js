0fbe58d99acfcb730453f90c12bc6f66f627fdfb
rodion2643 <rodionkulikov2502@gmail.com>
2026-06-26 09:16:39 +0300

    счмчсмчс

--- js/listings-config.js
+++ js/listings-config.js
@@ -1,5 +1,5 @@
 window.KWT_LISTINGS_CONFIG = {
-apiUrl:'https://script.google.com/macros/s/AKfycbwFxdDvtEL2SSAbaelnFlwNzz1DPaIjomuj17NAsYdPuSCyPZpVx6BDcvGeAteTTpi6/exec',
+apiUrl:'https://script.google.com/macros/s/AKfycbyP7vQHUGYwIBLmF-GsmdUBzQ9AkXXkAxJ0x--RzPT5KX0ewGlAPg6cx3raqCo2EIXs/exec',
   siteUrl: 'https://rodion2643.github.io/kwt_service/',
   cacheTtlMs: 120000,
 };
